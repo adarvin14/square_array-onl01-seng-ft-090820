d41abeaa9207b2a946a4e7910a099823d23b9c0a
Alex Darvin <amdarvin14@gmail.com>
2020-09-14 19:10:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,7 @@
 def square_array(array)
-<<<<<<< HEAD
   new_array = []
   array.each do |number|
   new_array << number**2
   end
   new_array
-=======
-  array.each do |number|
-    array = numbers**2
-    puts "#{array}"
-  end
-  square_array.each do |number**2|
->>>>>>> e38929f459fa5bb177b3ca31f0853cdffadebbed
 end
